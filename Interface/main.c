ba14e37924ae0c211e1e95b2640433aaa5bcc9fb
Lucas DUPORT <lucas.duport@epita.fr>
2022-09-21 21:13:19 +0200

    back button almost done

--- Interface/main.c
+++ Interface/main.c
@@ -1,45 +1,46 @@
 #include <gtk/gtk.h>
-/*#include <unistd.h>
-#include <string.h>
-#include <math.h>
-#include <ctype.h>*/
+#include <unistd.h>
+#include <stdio.h>
 
 GtkWindow *window;
-
 GtkWidget *fixed1;
-GtkWidget *button1;
 
-GtkImage *image1;
+GtkWidget *upload_button;
+GtkWidget *reload_button;
+
+GtkWidget *or_label;
+GtkImage *sudoku_image;
+GtkEntry *upload_entry;
 
 GtkBuilder *builder;
 
-int main (int argc, char *argv[])
+
+int load_menu ()
 {
 
-	gtk_init(&argc, &argv); //init Gtk
-	
+		
 	//establish contact with xml code used to adjust widget settings
 	
 	builder = gtk_builder_new_from_file("main.glade");
-
 	window = GTK_WINDOW(gtk_builder_get_object(builder,"window"));
-
 	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit),NULL);
 	
+
 	gtk_builder_connect_signals(builder, NULL);
 
 	fixed1 = GTK_WIDGET(gtk_builder_get_object(builder, "fixed1"));
 
-	button1 = GTK_WIDGET(gtk_builder_get_object(builder, "button1"));
+	upload_button = GTK_WIDGET(gtk_builder_get_object(builder, "upload_button"));
+	reload_button = GTK_WIDGET(gtk_builder_get_object(builder, "reload_button"));
+	gtk_widget_hide(reload_button);
 
-	image1 = GTK_IMAGE(gtk_builder_get_object(builder, "image1"));
 
-	entry1 = GTK_ENTRY(gtk_builder_get_object(builder, "entry1"));
+	upload_entry = GTK_ENTRY(gtk_builder_get_object(builder, "upload_entry"));
 
+	or_label= GTK_WIDGET(gtk_builder_get_object(builder, "or_label"));
 
 
 	gtk_widget_show(GTK_WIDGET (window));
-	
 	gtk_window_fullscreen(window);
 	gtk_window_set_title(window, "SUDOKU SOLVER");
 		
@@ -50,12 +51,58 @@ int main (int argc, char *argv[])
 
 }
 
-void on_button1_clicked()
+int main (int argc, char *argv[])
+{
+	gtk_init(&argc, &argv); //init Gtk
+	load_menu();
+
+	return EXIT_SUCCESS;
+
+}
+
+void display_image(const char *filename, int show)
+{//diplay filename image and deleting all widgets in argument
+
+	if (show)
+	{
+		sudoku_image = GTK_IMAGE(gtk_builder_get_object(builder, "sudoku_image"));
+	
+		gtk_image_set_from_file(GTK_IMAGE(sudoku_image),filename);
+
+		gtk_widget_show(reload_button);
+	}
+	else
+	{
+		gtk_image_set_from_file(sudoku_image, "");
+
+	}
+	
+	return;
+
+}
+
+void widget_displayer(GtkWidget **widgets, size_t widget_number, int show)
+{
+	if (show == 1)
+	{
+		for(size_t i = 0; i < widget_number; i++)
+			gtk_widget_show(GTK_WIDGET(widgets[i]));
+	}
+	else
+	{
+		for(size_t i = 0; i < widget_number; i++)
+			gtk_widget_hide(GTK_WIDGET(widgets[i]));
+
+	}
+	return;
+}
+
+
+
+void on_upload_button_clicked()
 {
 	GtkWidget *dialog;
- 
 	gint res;
-
 	dialog = gtk_file_chooser_dialog_new ("Open File",
                                       GTK_WINDOW (window),
                                       GTK_FILE_CHOOSER_ACTION_OPEN,
@@ -66,33 +113,46 @@ void on_button1_clicked()
                                       NULL);
 
 	res = gtk_dialog_run (GTK_DIALOG (dialog));
+
 	if (res == GTK_RESPONSE_ACCEPT)
   	{
-    		char *filename;
     		GtkFileChooser *chooser = GTK_FILE_CHOOSER (dialog);
-    		filename = gtk_file_chooser_get_filename (chooser);
+    		const char *filename = gtk_file_chooser_get_filename (chooser);	
 
-		gtk_widget_destroy(button1);
-                gtk_widget_destroy(entry1);
+		GtkWidget* to_destroy[10] =  {or_label,GTK_WIDGET(upload_button),GTK_WIDGET(upload_entry)};
 
-
-		gtk_image_set_from_file(image1,filename);
+		display_image(filename,1);
+		widget_displayer(to_destroy, 3, -1);
 
   	}
+
 	gtk_widget_destroy(dialog);
 }
 
 //gtk entry
-void on_entry1_past_clipboard()
+void on_upload_entry_activate()
 {
-	res = gtk_entry_get_text(); 
-	if (res != 0)
+	const char* filename = gtk_entry_get_text(upload_entry); 
+	if (access(filename,F_OK) == 0)
   	{
+		GtkWidget* to_destroy[50] =  {or_label,GTK_WIDGET(upload_button),GTK_WIDGET(upload_entry)};
 
-		gtk_widget_destroy(button1);
-                gtk_widget_destroy(entry1);
-		gtk_image_set_from_file(image1,res);
+		display_image(filename,1);
 
+		widget_displayer(to_destroy, 3, 0);
+
+		
   	}
+}
 
+void on_reload_button_clicked()
+{
+	
+	GtkWidget* to_revive[10] =  {or_label,GTK_WIDGET(upload_button),GTK_WIDGET(upload_entry)};
+	widget_displayer(to_revive,3,1);
+
+	GtkWidget* to_destroy[10] =  {GTK_WIDGET(reload_button)};
+	widget_displayer(to_destroy, 1, 0);
+
+	display_image("",0);
 }
