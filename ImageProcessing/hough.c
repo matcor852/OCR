9d9201a1c845467b0dcb8cf072b6dc409aaea9e8
Mattéo Baussart <matteo.baussart@epita.fr>
2022-09-29 16:06:09 +0200

    some line detection shit

--- ImageProcessing/hough.c
+++ ImageProcessing/hough.c
@@ -4,32 +4,58 @@
 #include "openImage.h"
 #define PI 3.141592654
 
-void getY(st x, st *_y, st *y_, st r, float _cos, float _sin, st height){
+void getY(st x, st *_y, st *y_, st r, float _cos, float _sin){
 	float value = (r - x * _cos) / _sin;
 	*_y = value - 2;
 	*y_ = value + 2;
-	if (*_y < 0)
-		*_y = 0;
-	else if (*y_ >= height)
-		*y_ = height - 1;
-	if (*y_ < 0)
-		*y_ = 0;
-	else if (*_y >= height)
-		*_y = height - 1;
 }
 
-void getX(st *_x, st *x_, st y, st r, float _cos, float _sin, st width){
+void getX(st *_x, st *x_, st y, st r, float _cos, float _sin){
 	float value = (r - y * _sin) / _cos;
 	*_x = value - 2;
 	*x_ = value + 2;
-	if (*_x < 0)
-		*_x = 0;
-	else if (*x_ >= width)
-		*x_ = width - 1;
-	if (*x_ < 0)
-		*x_ = 0;
-	else if (*_x >= width)
-		*_x = width - 1;
+}
+
+int XtoY(st x, st r, float _cos, float _sin){
+	return (r - x * _cos) / _sin;
+}
+
+int YtoX(st y, st r, float _cos, float _sin){
+	return (r - y * _sin) / _cos;
+}
+
+uchar *getLine(uchar **pixels, st width, st height, int r, int theta, int threshold, st *len){
+	float _sin = sin(theta * PI / 180);
+	float _cos = cos(theta * PI / 180);
+	if ((45 <= theta && theta <= 135) || (225 <= theta && theta <= 315)){
+		uchar *line = (uchar *)malloc(width * sizeof(uchar));
+		for (st x = 0; x < width; x++){
+			st y = XtoY(x, r, _cos, _sin);
+			if (y < 0 || y >= height){
+				line[x] = 0;
+				continue;
+			}
+			line[x] = pixels[y][x] > threshold ? 255 : 0;
+		}
+		*len = width;
+		return line;
+	}
+	uchar *line = (uchar *)malloc(height * sizeof(uchar));
+	for (st y = 0; y < height; y++){
+		st x = YtoX(y, r, _cos, _sin);
+		if (x < 0 || x >= width){
+			line[y] = 0;
+			continue;
+		}
+		line[y] = pixels[y][x] > threshold ? 255 : 0;
+	}
+	*len = height;
+	return line;
+}
+
+void smoothLine(uchar *line, st len, st min){
+	// TODO
+	return;
 }
 
 void test(void){
@@ -62,8 +88,12 @@ void test(void){
 				for (st x = 0; x < width; x++){
 					st _y;
 					st y_;
-					getY(x, &_y, &y_, r, _cos, _sin, height);
+					getY(x, &_y, &y_, r, _cos, _sin);
 					for (st y = _y; y <= y_; y++){
+						if (y >= height)
+							break;
+						if (y < 0)
+							y = 0;
 						if (fabs(r - x * _cos - y * _sin) < 1){
 							if (x > x_max)
 								x_max = x;
@@ -83,8 +113,12 @@ void test(void){
 				for (st y = 0; y < height; y++){
 					st _x;
 					st x_;
-					getX(&_x, &x_, y, r, _cos, _sin, width);
+					getX(&_x, &x_, y, r, _cos, _sin);
 					for(st x = _x; x <= x_; x++){
+						if (x >= width)
+							break;
+						if (x < 0)
+							x = 0;
 						if (fabs(r - x * _cos - y * _sin) < 1){
 							if (x > x_max)
 								x_max = x;
@@ -100,7 +134,7 @@ void test(void){
 					}
 				}
 			}
-			int value = (nb_pixels && (x_max - x_min > width / 4 || y_max - y_min > height / 4)) ? total * 256 / nb_pixels : 0;
+			int value = (nb_pixels && (x_max - x_min > width / 4 || y_max - y_min > height / 4)) ? total / nb_pixels : 0;
 			r_theta[r][theta] = value;
 			if (value > best_value){
 				best_r = r;
@@ -118,12 +152,40 @@ void test(void){
 		}
 		printf("\n");
 	}
-	printf("r = %zu, theta = %zu, value = %d\n", best_r, best_theta, best_value);
-	int p1x = 0, p1y = best_r / sin(best_theta * PI / 180);
-	int p2y = 0, p2x = best_r / cos(best_theta * PI / 180);
-	int p3x = width - 1, p3y = (best_r - p3x * cos(best_theta * PI / 180)) / sin(best_theta * PI / 180);
-	int p4y = 0, p4x = (best_r - p4y * sin(best_theta * PI / 180)) / cos(best_theta * PI / 180);
-	printf("p1 = (%d, %d); p2 = (%d, %d)\n", p1x, p1y, p2x, p2y);
-	printf("p3 = (%d, %d); p4 = (%d, %d)\n", p3x, p3y, p4x, p4y);
-	printf("'%c', '%c'\n", chars[0], chars[69]);
+	for (st i = 0; i < 20; i++){
+		int best_value = 0;
+		st best_r = 0, best_theta = 0;
+		int value;
+		for (st r = 0; r < r_max; r++)
+			for (st theta = 0; theta < 360; theta++){
+				value = r_theta[r][theta];
+				if (value > best_value){
+					best_value = value;
+					best_r = r;
+					best_theta = theta;
+				}
+			}
+		printf("r = %zu, theta = %zu, value = %d\n", best_r, best_theta, best_value);
+		int p1x = 0, p1y = best_r / sin(best_theta * PI / 180);
+		int p2y = 0, p2x = best_r / cos(best_theta * PI / 180);
+		int p3x = width - 1, p3y = (best_r - p3x * cos(best_theta * PI / 180)) / sin(best_theta * PI / 180);
+		int p4y = 0, p4x = (best_r - p4y * sin(best_theta * PI / 180)) / cos(best_theta * PI / 180);
+		printf("p1 = (%d, %d); p2 = (%d, %d)\n", p1x, p1y, p2x, p2y);
+		printf("p3 = (%d, %d); p4 = (%d, %d)\n\n", p3x, p3y, p4x, p4y);
+		st len;
+		uchar *line = getLine(pixels, width, height, best_r, best_theta, best_value, &len);
+		for (st i = 0; i < len; i++)
+			printf("%c ", line[i] ? '#' : '.');
+		printf("\n");
+		for (st r = best_r - 20; r <= best_r + 20; r++){
+			if (r >= r_max)
+				break;
+			if (r < 0)
+				continue;
+			for (st theta = best_theta - 20; theta <= best_theta + 20; theta++){
+				r_theta[r][theta % 360] = 0;
+			}
+		}
+		free(line);
+	}
 }
